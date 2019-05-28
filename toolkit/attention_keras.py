394470c2282b6c9936ab43a651523ed7e536221c
tuixing.zx <tuixing.zx@alibaba-inc.com>
2019-05-28 16:23:00 +0800

    add attention

--- toolkit/attention_keras.py
+++ toolkit/attention_keras.py
@@ -8,7 +8,7 @@ class Attention(Layer):
     def __init__(self, step_dim,
                  W_regularizer=None, b_regularizer=None,
                  W_constraint=None, b_constraint=None,
-                 bias=True, **kwargs):
+                 bias=True, return_sequences=False, **kwargs):
         self.supports_masking = True
         self.init = initializers.get('glorot_uniform')
 
@@ -21,6 +21,7 @@ class Attention(Layer):
         self.bias = bias
         self.step_dim = step_dim
         self.features_dim = 0
+        self.return_sequences = return_sequences
         super(Attention, self).__init__(**kwargs)
 
     def build(self, input_shape):
@@ -51,24 +52,32 @@ class Attention(Layer):
         features_dim = self.features_dim
         step_dim = self.step_dim
 
-        eij = K.reshape(K.dot(K.reshape(x, (-1, features_dim)),
-                        K.reshape(self.W, (features_dim, 1))), (-1, step_dim))
+        eij = K.reshape(K.dot(
+                                K.reshape(x, (-1, features_dim)),
+                                K.reshape(self.W, (features_dim, 1))
+                              ),
+                        (-1, step_dim)
+                        )
 
         if self.bias:
             eij += self.b
 
         eij = K.tanh(eij)
 
-        a = K.exp(eij)
+        step_weight = K.exp(eij)
 
         if mask is not None:
-            a *= K.cast(mask, K.floatx())
+            step_weight *= K.cast(mask, K.floatx())
 
-        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
-
-        a = K.expand_dims(a)
-        weighted_input = x * a
-        return K.sum(weighted_input, axis=1)
+        step_weight /= K.cast(K.sum(step_weight, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+        # dim from (step_weight) to (step_weight, 1)
+        step_weight = K.expand_dims(step_weight)
+        # element-wise product
+        weighted_input = x * step_weight
+        if self.return_sequences:
+            return weighted_input
+        else:
+            return K.sum(weighted_input, axis=1)
 
     def compute_output_shape(self, input_shape):
         return input_shape[0],  self.features_dim
